502b5656ca4ee38b6f6b9f3182877a43018fd3ee
banli17 <867889876@qq.com>
2020-05-13 16:19:10 +0800

    feat: 增加权限申请、apk 升级

--- lib/pages/search.dart
+++ lib/pages/search.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_template/api/home.dart';
+import 'package:r_upgrade/r_upgrade.dart';
+import 'package:easy_permission_validator/easy_permission_validator.dart';
 import '../components/dialog.dart';
 import '../components/datePicker.dart';
 import '../components/toast.dart';
@@ -46,6 +48,29 @@ class Search extends StatelessWidget {
             child: Text('提示信息'),
             onPressed: () async {
               MToast.info(msg: 'hello');
+            },
+          ),
+          RaisedButton(
+            child: Text('升级apk'),
+            onPressed: () async {
+              final permissionValidator = EasyPermissionValidator(
+                context: context,
+                appName: 'app名称',
+                appNameColor: Colors.red,
+                cancelText: '取消',
+                enableLocationMessage:
+                    'Debe habilitar los permisos necesarios para utilizar la acción.',
+                goToSettingsText: 'Ir a Configuraciones',
+                permissionSettingsMessage:
+                    'Necesita habilitar los permisos necesarios para que la aplicación funcione correctamente',
+              );
+              var result = await permissionValidator.storage();
+              if (result) {
+                await RUpgrade.upgrade(
+                    'http://gdown.baidu.com/data/wisegame/283e9789be54e63c/weixin_1560.apk',
+                    apkName: 'app-release.apk',
+                    isAutoRequestInstall: true);
+              }
             },
           ),
         ],
--- lib/pages/travel.dart
+++ lib/pages/travel.dart
@@ -1,25 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:fijkplayer/fijkplayer.dart';
+//import 'package:fijkplayer/fijkplayer.dart';
 
 class Travel extends StatelessWidget {
-  final FijkPlayer player = FijkPlayer();
+//  final FijkPlayer player = FijkPlayer();
 
   @override
   Widget build(BuildContext context) {
     @override
     void dispose() {
-      player.release();
+//      player.release();
     }
 
     return Scaffold(
         appBar: AppBar(
           title: Text('旅拍'),
         ),
-        body: Container(
-          alignment: Alignment.center,
-          child: FijkView(
-            player: player,
-          ),
-        ));
+        body: Container(alignment: Alignment.center, child: Text('hello')));
   }
 }
