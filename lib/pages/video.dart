033834591d62d92d0d7107364b8e7efd7f5baf26
banli17 <867889876@qq.com>
2020-05-15 18:17:04 +0800

    feat: 集成 fijkplayer

--- lib/pages/video.dart
+++ lib/pages/video.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:video_player/video_player.dart';
+import 'package:fijkplayer/fijkplayer.dart';
 
 class VideoPage extends StatefulWidget {
   @override
@@ -7,40 +7,21 @@ class VideoPage extends StatefulWidget {
 }
 
 class _VideoPageState extends State<VideoPage> {
-  VideoPlayerController _controller;
+  final FijkPlayer player = FijkPlayer();
 
   @override
   void initState() {
     super.initState();
-    _controller = VideoPlayerController.network(
-        'http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4')
-      ..initialize().then((_) {
-        // Ensure the first frame is shown after the video is initialized, even before the play button has been pressed.
-        setState(() {});
-      });
+    player.setDataSource('http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4',
+        autoPlay: true);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: _controller.value.initialized
-            ? AspectRatio(
-                aspectRatio: _controller.value.aspectRatio,
-                child: VideoPlayer(_controller),
-              )
-            : Container(),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            _controller.value.isPlaying
-                ? _controller.pause()
-                : _controller.play();
-          });
-        },
-        child: Icon(
-          _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
+        child: FijkView(
+          player: player,
         ),
       ),
     );
@@ -49,6 +30,6 @@ class _VideoPageState extends State<VideoPage> {
   @override
   void dispose() {
     super.dispose();
-    _controller.dispose();
+    player.release();
   }
 }
