6656c3c8fb14c276c31e98a1fca626c54fdb5da8
banli17 <867889876@qq.com>
2020-05-18 18:38:58 +0800

    feat: 添加手势等

--- lib/main.dart
+++ lib/main.dart
@@ -16,11 +16,44 @@ void main() {
   }
 }
 
-// 页面、组件、元素都是 Widget
-// StatelessWidget 相当于 Component
-// build 相当于 render
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
+  Brightness _brightness = Brightness.light;
+
+  @override
+  void initState() {
+    WidgetsBinding.instance.addObserver(this); // 添加监听器
+    super.initState();
+    setState(() {
+//      _brightness = Brightness.dark;
+    });
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    super.didChangeAppLifecycleState(state);
+    print('state = $state');
+    if (state == AppLifecycleState.paused) {
+      print('app进入后台');
+    } else if (state == AppLifecycleState.resumed) {
+      print('app 进入前台');
+    } else if (state == AppLifecycleState.inactive) {
+      // 不常用，应用程序处于非活动状态，并且未接受用户输入时调用，比如来了个电话
+    } else if (state == AppLifecycleState.detached) {
+      // 不常用，应用程序被挂起时调用，不会在 ios 上出发
+    }
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return FLToastProvider(
@@ -28,6 +61,7 @@ class MyApp extends StatelessWidget {
       color: Colors.red,
       title: 'flutter_名称',
       theme: ThemeData(
+        brightness: _brightness,
         primarySwatch: Colors.blue,
         pageTransitionsTheme: PageTransitionsTheme(builders: {
           TargetPlatform.android: CupertinoPageTransitionsBuilder(),
--- lib/pages/my.dart
+++ lib/pages/my.dart
@@ -1,6 +1,18 @@
 import 'package:flutter/material.dart';
 
-class My extends StatelessWidget {
+class My extends StatefulWidget {
+  @override
+  _MyState createState() => _MyState();
+}
+
+class _MyState extends State<My> {
+  double moveX = 0;
+  double moveY = 0;
+
+  _printMsg(String msg) {
+    print('msg : $msg');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -9,14 +21,41 @@ class My extends StatelessWidget {
       ),
       body: Column(
         children: <Widget>[
-          RaisedButton(
-            child: Text('返回'),
-            onPressed: () {
-              Navigator.pop(context, '哈哈');
-            },
+          GestureDetector(
+            onTap: () => _printMsg('点击'),
+            onDoubleTap: () => _printMsg('双击'),
+            onLongPress: () => _printMsg('长按'),
+            onTapCancel: () => _printMsg('取消'),
+            onTapUp: (e) => _printMsg('松开'),
+            onTapDown: (e) => _printMsg('按下'),
+            child: Text('用户操作'),
           ),
+          Column(
+            children: <Widget>[
+              Positioned(
+                left: moveX,
+                top: moveY,
+                child: GestureDetector(
+                    onPanUpdate: (e) => _doMove(e),
+                    child: Container(
+                      width: 72,
+                      height: 72,
+                      decoration: BoxDecoration(
+                          color: Colors.black,
+                          borderRadius: BorderRadius.circular(36)),
+                    )),
+              )
+            ],
+          )
         ],
       ),
     );
+  }
+
+  _doMove(DragUpdateDetails e) {
+    setState(() {
+      moveX += e.delta.dx;
+//      moveY += e.delta.dy;
+    });
   }
 }
